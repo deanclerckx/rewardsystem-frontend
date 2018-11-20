a2942f5fcfd07f2c453f20cfc71038613158a55d
Thomas Vansprengel <iamthadjawi@gmail.com>
2018-11-20 10:41:15 +0100

    Getrewards & claimrewards design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,12 @@ import { AuthGuard } from './_guards/auth.guard';
 //Importeer components
 import { LoginComponent } from './login/login.component';
 import { ClaimrewardsComponent } from './claimrewards/claimrewards.component';
-
+import { GetrewardsComponent} from './getrewards/getrewards.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
     { path: 'claimrewards', component: ClaimrewardsComponent },
+    { path: 'getrewards', component: GetrewardsComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '/login' }
