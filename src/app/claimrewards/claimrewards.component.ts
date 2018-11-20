d52e2b0a9721e9f1df0532844feb61c46a2826f0
Stijn Geens <stijn.geens@outlook.be>
2018-11-20 11:32:17 +0100

    Add Reward Service. Use Reward Service in claimrewards Component. Fix issue in index.html.

--- src/app/claimrewards/claimrewards.component.ts
+++ src/app/claimrewards/claimrewards.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RewardService } from '../_services/reward.service';
+import { Reward } from '../_models/reward';
 
 @Component({
   selector: 'app-claimrewards',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./claimrewards.component.css']
 })
 export class ClaimrewardsComponent implements OnInit {
+  rewards: Reward[];
 
-  constructor() { }
+  constructor(public rewardService: RewardService) { }
 
   ngOnInit() {
+    this.rewardService.getAll().subscribe(rewards => this.rewards = rewards);
   }
-
 }
