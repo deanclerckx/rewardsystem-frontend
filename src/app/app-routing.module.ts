3066fe3d5d5920cb5a3f0d4a0b7547f15d350dcc
Thomas Vansprengel <iamthadjawi@gmail.com>
2018-11-20 08:50:27 +0100

    Front-end Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './_guards/auth.guard';
+import {RewardsComponent} from './rewards/rewards.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+    { path: 'rewards', component: RewardsComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '/login' }
