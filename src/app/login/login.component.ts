ecca98c34f009bd5796a0c3731c5ba2d4e752092
Thomas Vansprengel <iamthadjawi@gmail.com>
2018-11-23 11:52:15 +0100

    Hernoemen homepage in routing en paginatitels

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -46,7 +46,7 @@ export class LoginComponent implements OnInit {
             .subscribe(
                 data => {
                     this.alertService.success('Login successful', true);
-                    this.router.navigate(['/homepage']);
+                    this.router.navigate(['/overview']);
                     window.location.reload();
                 },
                 error => {
