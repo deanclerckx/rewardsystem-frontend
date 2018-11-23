40160c4fff68a8b5762560f2d144867cb8bcceda
Wim Naudts <wimnaudts@yahoo.com>
2018-11-23 11:47:17 +0100

    Change points after ordering

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -47,7 +47,6 @@ export class LoginComponent implements OnInit {
                 data => {
                     this.alertService.success('Login successful', true);
                     this.router.navigate(['/homepage']);
-                    window.location.reload();
                 },
                 error => {
                     this.alertService.error(error);
