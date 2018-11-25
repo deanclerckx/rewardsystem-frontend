1d093b8f76321afc81f95d11cedcaae859aca3e2
Wim Naudts <wimnaudts@yahoo.com>
2018-11-25 17:32:16 +0100

    deployment test

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/reward-system'));
+//app.use(express.static(__dirname + '/dist/reward-system'));
+app.use(express.static('/dist/reward-system'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/reward-system/index.html'));
+res.sendFile(path.join('/dist/reward-system/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
